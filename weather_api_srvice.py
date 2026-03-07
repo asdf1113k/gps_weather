1fd353ee3e95d9a5f18c34054d6327641dc6a71e
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-07 12:58:29 +0300

    переминовал переменую responce на responce_api_openweather

--- weather_api_srvice.py
+++ weather_api_srvice.py
@@ -3,11 +3,11 @@ import requests as rq
 from my_token import TOKEN
 # https://api.openweathermap.org/data/2.5/weather?lat=55.7&lon=37.5&appid=7549b3ff11a7b2f3cd25b56d21c83c6a&lang=ru&units=metric
 loc = json_ipinfo["loc"].strip(",")
-responce = rq.get(f"https://api.openweathermap.org/data/2.5/weather?lat={loc[0]}&lon={loc[1]}&appid={TOKEN}&lang=ru&units=metric")
+responce_api_openweather = rq.get(f"https://api.openweathermap.org/data/2.5/weather?lat={loc[0]}&lon={loc[1]}&appid={TOKEN}&lang=ru&units=metric")
 
 
 if __name__ == "__main__":
-    print(responce)
-    json_openweathermap = responce.json()
+    print(responce_api_openweather.status_code)
+    json_openweathermap = responce_api_openweather.json()
     for key in json_openweathermap:
         print(key, ":", json_openweathermap[key])
