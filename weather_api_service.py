608077f2fe8cdb83837119e714369d7434ef5502
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-07 18:19:43 +0300

    исправлено: ввод координат, я не заметил а тип который я вставлял в api запрос был str а не list, а нужен был List исправил

--- weather_api_service.py
+++ weather_api_service.py
@@ -2,12 +2,12 @@ from coordinates import json_ipinfo
 import requests as rq
 from my_token import TOKEN
 # https://api.openweathermap.org/data/2.5/weather?lat=55.7&lon=37.5&appid=7549b3ff11a7b2f3cd25b56d21c83c6a&lang=ru&units=metric
-loc = json_ipinfo["loc"].strip(",")
+loc:str = json_ipinfo["loc"]
+loc:list = loc.split(",")
 responce_api_openweather = rq.get(f"https://api.openweathermap.org/data/2.5/weather?lat={loc[0]}&lon={loc[1]}&appid={TOKEN}&lang=ru&units=metric")
-json_openweather = responce_api_openweather.json()
+json_openweathermap = responce_api_openweather.json()
 
 if __name__ == "__main__":
     print(responce_api_openweather.status_code)
-    json_openweathermap = responce_api_openweather.json()
     for key in json_openweathermap:
         print(key, ":", json_openweathermap[key])
