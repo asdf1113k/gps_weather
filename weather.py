4af43a8cbaf302f70dbca479c8d7d09c724fa613
Maxim Bronnykov(TypeErr0r) <asdf1113k.gmail.com>
2026-03-08 13:36:49 +0300

    думал сделать отдельный файл для сортировки json так как обращаться по слишком длиным именам не удобно, но добрый человек подсказал что можно сделать класс weather

--- weather.py
+++ weather.py
@@ -5,16 +5,23 @@ from coordinates import json_ipinfo
 
 from colorama import init, Fore
 
+class weather:
+    def __init__(self, json_ipinfo, json_api_weather):
+        self.region = json_ipinfo['region']
+        self.city = json_ipinfo['city']
+        self.temp = json_api_weather['main']['temp']
+        self.weather_description = json_api_weather['weather'][0]['description']
+        
+    def print_weather(self):
+        print(Fore.GREEN + f'погода в {self.region}, {self.city}')
+        print(Fore.LIGHTBLUE_EX + f'{self.weather_description}'.center(25))
+        print(Fore.LIGHTYELLOW_EX + f'температура: {self.temp}'.center(25))
+
 init(autoreset=True)
 if __name__ == "__main__":
     weather_great()
     # ip скрыт в weather_api_service
     # print(json_ipinfo)
     # print(json_openweathermap)
-    print(Fore.GREEN + f'погода в {json_ipinfo["region"]}, {json_ipinfo["city"]}')
-    print(Fore.LIGHTBLUE_EX + f'{json_openweathermap['weather'][0]['description']}'.center(25))
-    print(Fore.LIGHTYELLOW_EX + f'{json_openweathermap['main']}'.center(25),'\n'
-          f'температура: {json_openweathermap['main']['temp']}'.center(25))
-    
-
-
+    wt = weather(json_ipinfo, json_openweathermap)
+    wt.print_weather()
