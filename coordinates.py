3f1a077e4968208d6497f8c922908a60f25a6d43
Maxim Bronnykov(TypeErr0r) <asdf1113k@gmail.com>
2026-03-18 14:08:05 +0300

    added new requirements gpsd-py3 and windsk

--- coordinates.py
+++ coordinates.py
@@ -1,1 +1,17 @@
+# import gpsd
+# gpsd.connect()
 
+# packet = gpsd.get_current()
+# if packet.mode >= 2:
+#      print(f"Широта: {packet.lat}, Долгота: {packet.lon}")
+
+import asyncio
+import winsdk.windows.devices.geolocation as wdg
+
+async def get_location():
+    locator = wdg.Geolocator()
+    pos = await locator.get_geoposition_async()
+    return pos.coordinate.latitude, pos.coordinate.longitude
+
+lat, lon = asyncio.run(get_location())
+print(f"Широта: {lat}, Долгота: {lon}")
